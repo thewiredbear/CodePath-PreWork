da428f0ea7ec81093a80b0cbce37dbab219bbf53
thewiredbear <parthbhardwaj1996@gmail.com>
2015-12-13 20:29:15 -0500

    Update Main
    Updating the main folder

---
+++ SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Parth Bhardwaj on 12/13/15.
+//  Copyright © 2015 Parth Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: NSObject {
+
+}
